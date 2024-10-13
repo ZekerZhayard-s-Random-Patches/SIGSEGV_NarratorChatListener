98db9e5012789e32014e85038cf03ee0130087e9
ZekerZhayard <trees1000@qq.com>
2024-10-13 18:01:56 +0800

    1.18.2-1.0

---
+++ src/main/java/io/github/zekerzhayard/sigsegv_narratorchatlistener/SIGSEGV_NarratorChatListener.java
@@ -0,0 +1,9 @@
+package io.github.zekerzhayard.sigsegv_narratorchatlistener;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("sigsegv_narratorchatlistener")
+public class SIGSEGV_NarratorChatListener {
+
+}
